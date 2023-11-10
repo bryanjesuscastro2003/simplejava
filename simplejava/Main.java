58d674b4c6110373c94a6897b475a17c16ea439a
bryanjesuscastro2003 <jesusbryam624@gmail.com>
2023-11-10 07:54:51 -0600

    v1

---
+++ simplejava/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("Hello, World!"); // Example print statement
+
+        // You can add more code as needed for your application
+    }
+}
