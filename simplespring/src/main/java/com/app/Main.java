917733f7e1c1871d937f11a82318aeb2071ddc70
bryanjesuscastro2003 <jesusbryam624@gmail.com>
2023-11-10 09:44:46 -0600

    v2

--- simplespring/src/main/java/com/app/Main.java
+++ simplespring/src/main/java/com/app/Main.java
@@ -5,7 +5,7 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
 public class Main {
     public static void main(String[] args) {
        SpringApplication.run(DemoApplication.class, args);
